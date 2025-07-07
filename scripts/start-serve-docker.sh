0721c63eb160c42926303b01ffb5f7d3db5904cb
장정우 <enterfive@gmail.com>
2025-07-07 16:04:53 +0900

    202500707 비전공자도 이해할 수 있는 CI/CD 입문·실전 - 방법 5 - 컨테이너 기반 + 확장성을 고려한 프로젝트에서 많이 쓰는 CI/CD 구축 방법 (Docker, CodeDeploy)

--- scripts/start-serve-docker.sh
+++ scripts/start-serve-docker.sh
@@ -2,6 +2,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop github-action-springboot-example || true
 docker rm github-action-springboot-example || true
-docker pull 231573067935.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-springboot-example/github-action-springboot-example:latest
-docker run -d --name instagram-server -p 80:80 231573067935.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-springboot-example/github-action-springboot-example:latest
+docker pull 231573067935.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-springboot-example:latest
+docker run -d --name instagram-server -p 80:80 231573067935.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-springboot-example:latest
 echo "--------------- 서버 배포 끝 -----------------"
